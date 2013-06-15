95c21aedeca85e6bb2619ce42bf9d1e06762e5d7
Raphaël Nanni <raphael.nanni@gmail.com>
2013-06-15 13:41:00 +0200

    Initial commit
    App bootstrap based on HTML5BP

---
+++ js/main.js
@@ -0,0 +1,97 @@
+$(document).ready(function(){
+
+    var colorScheme = {
+        white : "ffeeaa",
+        blue : "157dde",
+        black : "222222",
+        red : "c91800",
+        green : "318428"
+    }
+
+    var plane = {
+
+        options: {
+            hue : "0.2"
+        },
+
+        init: function(selector) {
+            this.el = $(selector);
+            this.colors = [];
+
+            this.setElements();
+            this.setEvents();
+        },
+
+        setElements: function() {
+            this.$colorSelector = this.el.find('.edit .mana li');
+        },
+
+        setEvents: function() {
+            var _this = this;
+
+            this.$colorSelector.on('click', function(e){
+                var color = $(e.target).data('color');
+                _this.selectColor(color);
+                _this.setBackgroundColors()
+            })
+        },
+
+        selectColor: function(color) {
+            var registered = false,
+                id;
+
+            // iterate through colors array to check if already selected
+            $.each(this.colors, function(index, value){
+                if (value == color) {
+                    registered = true;
+                    id = index;
+                }
+            });
+
+            // toggle value in colors array
+            if (registered == true) {
+                this.colors.remove(id);
+            } else {
+                this.colors.push(color);
+            }
+
+            console.log(this.colors);
+        },
+
+        setBackgroundColors: function() {
+            var cssParams = [],
+                cssValue = "";
+            if (this.colors.length == 1) {
+                var color = this.colors[0];
+                cssValue = '-webkit-linear-gradient(-45deg, ' + color + ' 0%,' + this.getColorLuminance(color, this.options.hue)+ ' 100%)';
+            } else {
+                cssValue = '';
+            }
+
+            this.el.css('background', cssValue)
+        },
+
+        getColorLuminance: function(hex, lum) {
+
+        	// validate hex string
+        	hex = String(hex).replace(/[^0-9a-f]/gi, '');
+
+        	if (hex.length < 6) { // 3 digits hexa value, convert to 6 digits
+        		hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+        	}
+        	lum = lum || 0;
+
+        	// convert to decimal and change luminosity
+        	var rgb = "#", c, i;
+        	for (i = 0; i < 3; i++) {
+        		c = parseInt(hex.substr(i*2,2), 16);
+        		c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+        		rgb += ("00"+c).substr(c.length);
+        	}
+        	return rgb;
+        }
+    }
+
+    plane.init('.player');
+
+});
---
+++ js/plugins.js
@@ -0,0 +1,31 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
